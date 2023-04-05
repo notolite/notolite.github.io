503e5a09f8d3050a2c3b320a277a726109714d4a
notolyte <mina37s.sumomo@gmail.com>
2023-04-05 17:31:45 +0900

    言語切替時の背景画像の高さ

--- lang.js
+++ lang.js
@@ -3,14 +3,14 @@ window.onload = () => {
     toJpa();
 }
 function changelang() {
-    if (window.innerWidth < 440) {
-        artheight = document.getElementsByTagName("article")[0].offsetHeight;
-        document.getElementById("bg").style.height = artheight / 3 + "px";
-    }
     Array.from(document.getElementsByClassName("ln")).forEach(elm => { elm.style.display = "none"; });
     Array.from(document.getElementsByClassName(lang)).forEach(elm => { elm.style.display = "block"; });
     Array.from(document.getElementsByTagName("button")).forEach(elm => { elm.style.borderBottom = null; });
     document.getElementById(lang).style.borderBottom = "1px white solid";
+    if (window.innerWidth < 440) {
+        artheight = document.getElementsByTagName("article")[0].offsetHeight;
+        document.getElementById("bg").style.height = artheight / 3 + "px";
+    }
 }
 function toJpa() {
     lang = "jp";
