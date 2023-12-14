0393a3e9986e495fefa0b7fca1f44a1479fde5a7
notolyte <mina37s.sumomo@gmail.com>
2023-12-14 11:53:34 +0900

    --

--- lang.js
+++ lang.js
@@ -1,11 +1,11 @@
 window.onload = () => {
     changelang("jp");
+    calcAge();
 }
 function changelang(lang) {
     Array.from(document.getElementsByClassName("ln")).forEach(elm => { elm.style.display = "none"; });
     Array.from(document.getElementsByClassName(lang)).forEach(elm => { elm.style.display = "block"; });
     Array.from(document.getElementsByTagName("button")).forEach(elm => { elm.style.borderBottom = null; });
-    document.getElementById(lang).style.borderBottom = "1px white solid";
     let ttl;
     switch (lang) {
         case "fr":
@@ -26,4 +26,19 @@ function changelang(lang) {
         artheight = document.getElementsByTagName("article")[0].offsetHeight;
         document.getElementById("bg").style.height = artheight + "px";
     }
+}
+
+function calcAge() {
+    const currentDate = new Date();
+    let year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+    const date = currentDate.getDate();
+    if (month == 11 && date > 6) {
+        year += 1;
+    }
+    const age = year - 2003;
+    Array.from(document.getElementsByClassName("age")).forEach(elm => {
+        elm.innerText = age;
+    });
+    console.log(age);
 }
