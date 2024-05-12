febcfdccaae5d6e3999af35c7a6fc90f66c3f81b
notolyte <mina37s.sumomo@gmail.com>
2024-05-12 16:48:09 +0900

    debug

--- pictures/newpic.bat
+++ pictures/newpic.bat
@@ -1,7 +1,9 @@
+cd pictures
 set cmd="dir /a-d /b | find /c /v """
 for /f %%a in ('%cmd%') do (
     set fileCount=%%a
 )
+cd ..
 set /a  imageFiles = %fileCount% - 3
 echo const num ^= %imageFiles%; > picnum.js
 git add *
